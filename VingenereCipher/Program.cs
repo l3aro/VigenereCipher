2f9badfceae5366caf161093fca6af51daab3240
l3lackheart <neweragon2@yahoo.com>
2017-01-02 14:31:47 +0700

    Resist blank plain text

--- VingenereCipher/Program.cs
+++ VingenereCipher/Program.cs
@@ -10,6 +10,21 @@ namespace VingenereCipher
             string plainText;
             Console.Write("Nhap van ban can ma hoa: ");
             plainText = Console.ReadLine();
+            bool isContinue = false;
+            if (plainText == "")
+            {
+                isContinue = true;
+            }
+            while (isContinue)
+            {
+                Console.Write("Nhap it nhat 1 ky tu de ma hoa: ");
+                plainText = Console.ReadLine();
+                if (plainText != "")
+                {
+                    isContinue = false;
+                }
+            }
+            
 
             Console.Write("Nhap vao tu khoa, bo trong neu muon dung tu khoa mac dinh: ");
             VingenereCipher.keyword = Console.ReadLine();
